05f682bb0706620a5bd34a781c600e323a23a341
Lucas Cordeiro <cordeiroojlucas@gmail.com>
2025-08-01 10:55:18 -0300

    Atividade

---
+++ Atividades T.i/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    
+    char nome[100];
+    
+    printf("digite seu nome: ");
+    scanf("%s",nome );
+    printf("Olá %s! seja bem vindo ao mundo da programação em C ", nome);
+    
+}
