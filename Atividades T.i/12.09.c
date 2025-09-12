9c51d416a20932206718f48578ed1200c2bff537
Lucas cordeiro <cordeiroojlucas@gmail.com>
2025-09-12 10:21:47 -0300

    Atividade dia 12/09

---
+++ Atividades T.i/12.09.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+
+
+float chamar() {
+    float n;
+    printf("Digite o valor: ");
+    scanf("%f", &n);
+    return n;
+}
+
+
+void main() {
+    char operacao;
+    float n1, n2, resultado;
+
+    while (1) {
+        printf("================================================\n");
+        printf("             C A L C U L A D O R A              \n");
+        printf("================================================\n");
+        printf("Digite a operação desejada (+, -, *, /) ou s para sair: ");
+        scanf(" %c", &operacao);
+
+        if (operacao == 's' ) {
+            printf("Saindo...\n");
+            break;
+        }
+
+        if (operacao != '+' && operacao != '-' && operacao != '*' && operacao != '/') {
+            printf("Operação inválida! Tente novamente.\n\n");
+            continue;
+        }
+
+        n1 = chamar();
+        n2 = chamar();
+
+        switch (operacao) {
+            case '+':
+                resultado = n1 + n2;
+                break;
+            case '-':
+                resultado = n1 - n2;
+                break;
+            case '*':
+                resultado = n1 * n2;
+                break;
+            case '/':
+                resultado = n1 / n2;
+                break;
+        }
+
+        printf("Resultado: %.2f\n\n", resultado);
+    }
+
+    
+}
