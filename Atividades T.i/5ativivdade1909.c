46f05131e0bab7b91111867b8c9c85ddcce92db7
Lucas Cordeiro <cordeiroojlucas@gmail.com>
2025-09-19 09:53:21 -0300

    Atividades

---
+++ Atividades T.i/5ativivdade1909.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+void main() {
+    int vetor[8];
+    int maior, menor;
+
+    for(int i = 0; i < 8; i++) {
+        printf("Digite o valor de %d: ", i + 1);
+        scanf("%d", &vetor[i]);
+    }
+
+    maior = vetor[0];
+    menor = vetor[0];
+
+    for(int i = 1; i < 8; i++) {
+        if(vetor[i] > maior) {
+            maior = vetor[i];
+        }
+        if(vetor[i] < menor) {
+            menor = vetor[i];
+        }
+    }
